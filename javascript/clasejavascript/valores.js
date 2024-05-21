db81dd104ae3ebc6e55814098eaa794bf75ac685
Sergio Castañeda <duncan9276@hotmail.com>
2024-05-21 00:22:04 -0500

    Add files via upload

---
+++ javascript/clasejavascript/valores.js
@@ -0,0 +1,14 @@
+
+"Generando Comentario"
+/*Generando Comentario*/
+
+"Valor Booleano"
+true
+false
+
+"Valores Vacios"
+null
+undefined
+
+"Valores no primitivos"
+{ nombre: "Diego" } "valor tipo objeto"
